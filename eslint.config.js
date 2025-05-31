ab7136f806cb018d4784adf5934e300d56d65540
yukineowq <58840953+yukineowq@users.noreply.github.com>
2025-06-01 00:15:56 +0800

    Changed to tailwindcss v3 and still configuring for tailwind.config file

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  extends: ["next/core-web-vitals", "next"],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: "module",
+  },
+  rules: {
+    // your overrides here
+  },
+};
