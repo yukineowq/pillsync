8d6c9f8e9ab824fea0c50a62e13f88cd7edda785
yukineowq <58840953+yukineowq@users.noreply.github.com>
2025-06-03 13:56:49 +0800

    Added footer and motion variants from framer motion

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,12 @@ import ImageSwiper from "@/components/ImageSwiper";
 import StepsSection from "@/components/StepSection";
 import FeatureSection from "@/components/FeatureSection";
 import Spline from "@splinetool/react-spline";
+import { motion } from "framer-motion";
+import {
+  headerVariant,
+  subheaderVariant,
+  rotateFromRightVariant,
+} from "@/motionVariants/HomePage";
 
 export default function Home() {
   return (
@@ -37,7 +43,7 @@ export default function Home() {
                 height={36}
               />
             </div>
-            <div className="hidden md:flex md:gap-x-8 md:text-body-sm md:font-medium">
+            <div className="hidden md:flex md:gap-x-8 md:text-body-sm md:font-semibold">
               <CustomLink className="text-zinc-700" href="#product">
                 Our Product
               </CustomLink>
@@ -46,40 +52,61 @@ export default function Home() {
         </div>
 
         {/* main hero section */}
-        <div className="bg-zinc-50">
-          <div className="mx-auto max-w-7xl px-8 pt-4 lg:flex lg:gap-40 lg:py-20">
-            {/* mobile hero section */}
-            <section className="mx-auto mt-8 max-w-7xl">
-              <div className="text-h4 font-bold text-zinc-700 lg:text-h1">
-                Manage your {""}
-                <span className="text-h4 font-bold bg-gradient-to-r from-teal-200 to-teal-500 text-transparent bg-clip-text lg:text-h1">
-                  Medication Easily{" "}
-                  <span className="whitespace-nowrap">and Smartly</span>
-                </span>
-              </div>
+        <div className="mx-auto max-w-7xl px-8 pt-4 lg:flex lg:gap-40 lg:py-20">
+          {/* mobile hero section */}
+          <section className="mx-auto mt-8 max-w-7xl">
+            <motion.h1
+              className="text-h4 font-bold text-zinc-700 lg:text-h1"
+              variants={headerVariant}
+              initial="hidden"
+              animate="visible"
+            >
+              Manage your {""}
+              <span className="text-h4 font-bold bg-gradient-to-r from-teal-200 to-teal-500 text-transparent bg-clip-text lg:text-h1">
+                Medication Easily{" "}
+                <span className="whitespace-nowrap">and Smartly</span>
+              </span>
+            </motion.h1>
 
-              <p className="mt-5 text-body-lg font-medium text-gray-500 lg:mt-10 lg:text-h3">
-                Enhances patient’s experience with AI-powered pharmaceutical
-                service
-              </p>
-            </section>
+            <motion.p
+              className="mt-5 text-body-lg font-medium text-gray-500 lg:mt-10 lg:text-h3"
+              variants={subheaderVariant}
+              initial="hidden"
+              animate="visible"
+            >
+              Enhances patient’s experience with AI-powered pharmaceutical
+              service
+            </motion.p>
+          </section>
 
-            {/* 3D model section */}
-            <section className="mx-auto max-w-7xl mt-8 mb-8 lg:mt-0 lg:mb-0 h-[300px] md:h-[400px] lg:h-[650px] w-full">
-              <div className="w-full h-full">
-                <Spline
-                  scene="https://prod.spline.design/Zzm6PH8x0u7-YPak/scene.splinecode"
-                  className="w-full h-full"
-                  style={{ background: "transparent" }}
-                />
-              </div>
-            </section>
-          </div>
+          {/* 3D model section */}
+          <section className="mx-auto max-w-7xl mt-8 mb-8 lg:mt-0 lg:mb-0 h-[300px] md:h-[400px] lg:h-[650px] w-full">
+            <motion.div
+              className="w-full h-full"
+              variants={rotateFromRightVariant}
+              initial="hidden"
+              animate="visible"
+            >
+              <Spline
+                scene="https://prod.spline.design/Zzm6PH8x0u7-YPak/scene.splinecode"
+                className="w-full h-full"
+                style={{ background: "transparent" }}
+              />
+            </motion.div>
+          </section>
         </div>
 
         {/* Use case scenarios section */}
         <section className="mx-auto max-w-7xl px-8 lg:flex lg:py-20">
-          <ImageSwiper />
+          <motion.span
+            variants={subheaderVariant}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            className="w-full h-full"
+          >
+            <ImageSwiper />
+          </motion.span>
         </section>
 
         {/* How it works section */}
@@ -101,11 +128,65 @@ export default function Home() {
         {/* Video section */}
         <section className="mx-auto max-w-7xl px-8 py-8 lg:flex lg:py-20">
           <video width="1200" height="720" controls>
-            <source src="video/test.mp4" type="video/mp4"/>
+            <source src="video/test.mp4" type="video/mp4" />
           </video>
         </section>
 
-        
+        {/* What's in our box section */}
+        <section className="mx-auto max-w-7xl px-8 py-8 lg:py-20">
+          <div className="text-center text-h4 font-bold text-zinc-700 lg:text-h1">
+            What's in our box
+          </div>
+          <div className="flex flex-col gap-y-12 lg:flex-row lg:justify-center lg:items-center lg:gap-x-12 pt-8 lg:pt-16">
+            <div>
+              <div className="rounded-md p-4 shadow-lg transition-all duration-300 hover:shadow-2xl">
+                <Image
+                  src="/images/pillsync-packaging.png"
+                  alt="Box packaging"
+                  width={600}
+                  height={600}
+                />
+              </div>
+              <div className="text-center mt-8 lg:mt-16">
+                <h3 className="text-h5 font-medium text-zinc-700">
+                  Outer packaging
+                </h3>
+              </div>
+            </div>
+            <div>
+              <div className="rounded-md p-4 shadow-lg transition-all duration-300 hover:shadow-2xl">
+                <Image
+                  src="/images/pillsync-packaging.png"
+                  alt="Pharmacy pack"
+                  width={600}
+                  height={600}
+                />
+              </div>
+              <div className="text-center mt-8 lg:mt-16">
+                <h3 className="text-h5 font-medium text-zinc-700">
+                  Pharmacy pack
+                </h3>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <footer className="mt-20 flex flex-col items-start lg:items-center gap-y-14 bg-teal-50 px-12 py-12">
+          <div className="">
+            <Image
+              src="/svgs/pillsync-logo.svg"
+              alt="Pillsync logo"
+              width={114}
+              height={36}
+            />
+          </div>
+          <p className="text-left lg:text-center text-body-lg text-medium text-gray-500">
+            Manage your medication easily and smartly.
+          </p>
+          <p className="text-left lg:text-center text-body-sm text-medium text-gray-500">
+            © 2025 PillSync. All rights reserved.
+          </p>
+        </footer>
       </main>
     </div>
   );
